bac3d53ab732b66114a01cbd244ef1ad5a7f55d3
deweechi <chipdeweese@gmail.com>
2020-09-18 13:17:58 -0400

    Update Hero Text Size

--- docs/.vuepress/theme/components/Hero.vue
+++ docs/.vuepress/theme/components/Hero.vue
@@ -3,11 +3,11 @@
     <v-layout justify-end align-end column pa-5>
       <div
         class="font-weight-black white--text text-xs-right"
-        :class=" this.$vuetify.breakpoint.name == 'xs' ? 'display-1' : 'display-4' "
+        :class=" this.$vuetify.breakpoint.name == 'xs' ? 'display-4' : 'display-1' "
       >{{ $page.frontmatter.name }}</div>
       <div
         class="display-4 font-weight-black primary--text text-xs-right mb-3"
-        :class=" this.$vuetify.breakpoint.name == 'xs' ? 'display-1' : 'display-4' "
+        :class=" this.$vuetify.breakpoint.name == 'xs' ? 'display-4' : 'display-1' "
       >{{ $page.frontmatter.occupation }}</div>
     </v-layout>
   </v-container>
