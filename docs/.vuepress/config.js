ed1443c2cb66ab7c8844410036a87717c9b2e7d9
deweechi <chipdeweese@gmail.com>
2022-01-17 12:40:10 -0500

    Update to include google tag manager

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,8 @@ module.exports = {
   description: "This is the home page for Chip DeWeese. This site was built with VuePress, Vuetify and unSplash.",
   head: [
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons'}],
-    ['link', { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css'}]
-   
+    ['link', { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css'}],
+    ['script', {}, `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TZXTNR2');`],   
 ],
 base: '/',
 
